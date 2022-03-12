66f6520b9ba053adffd36c8ad8db85845c858bb8
juniorCoderKrithika <72335062+juniorCoderKrithika@users.noreply.github.com>
2022-03-12 19:59:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,29 @@ function startClassification(){
 function modelLoaded(){
     classifier.classify(gotResults);
 }
-function gotResults(errror,results){
-    console.log(results);
+function gotResults(error,results){
+    if (error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("result_label").innerHTML="I can hear - " + results[0].label;
+        document.getElementById("result_confidence").innerHTML="Accuracy - " + (results[0].confidence * 100).toFixed(2)+" %";
+
+        
+        img=document.getElementById("image_display");
+
+        if(results[0].label == "Barking"){
+            img.src="puppy.png";
+        }else if(results[0].label == "Meowing"){
+            img.src="kitten.png";
+        }
+        else if(results[0].label == "Mooing"){
+            img.src="cow.png";
+        }
+        else if(results[0].label == "Roaring"){
+            img.src="lion.png";
+        }else{
+            img.src="ear.png";
+        }
+    }
 }
